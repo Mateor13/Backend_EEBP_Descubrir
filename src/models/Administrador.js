8a7ce9b35a560844241582a5c41e5611fe8a6126
Mateor13 <mateo.torres@epn.edu.ec>
2024-12-14 19:23:27 -0500

    nombre coleccion

--- src/models/Administrador.js
+++ src/models/Administrador.js
@@ -1,7 +1,7 @@
-import {Collection, Schema, model} from 'mongoose'
+import {Schema, model} from 'mongoose'
 import bcrypt from 'bcryptjs'
 
-const adminSchema = new Schema({
+const administradorSchema = new Schema({
     nombre:{
         required: true,
         type: String
@@ -30,26 +30,26 @@ const adminSchema = new Schema({
     estado:{
         default: true,
         type: Boolean
-    }
+   }
 },
     {
-        timestamps: true
-    },
-    {Collection: 'administradores'}
+        timestamps: true,
+        collection: 'administradores'
+    }
 )
 
-adminSchema.methods.encriptarPassword = async password => {
+administradorSchema.methods.encriptarPassword = async password => {
     const salt = await bcrypt.genSalt(10)
     return await bcrypt.hash(password, salt)
 }
 
-adminSchema.methods.compararPassword = async function(password){
+administradorSchema.methods.compararPassword = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
-adminSchema.methods.generarToken = async function(){
+administradorSchema.methods.generarToken = async function(){
     const token = Math.random().toString(36).slice(2)
     return token
 }
 
-export default model('Administrador', adminSchema);
+export default model('Administrador', administradorSchema);
