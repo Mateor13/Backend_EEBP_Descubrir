bb0e082f851e30361af9b0af361224f5fd900f02
Mateor13 <mateo.torres@epn.edu.ec>
2024-12-22 16:56:45 -0500

    notas

--- src/routers/profesor_routes.js
+++ src/routers/profesor_routes.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import { comprobarTokenPassword, confirmarCuenta, loginProfesor, nuevoPassword, recuperarPassword, registrarEstudiantes, registrarProfesor, registroAsistenciaEstudiantes } from '../controllers/profesor_controller.js'
+import { comprobarTokenPassword, confirmarCuenta, loginProfesor, nuevoPassword, recuperarPassword, registrarEstudiantes, registrarProfesor, registroAsistenciaEstudiantes, subirNotasEstudiantes } from '../controllers/profesor_controller.js'
 import { verificarAutenticacion, verificarRolAdmin, verificarRolProfesor } from '../helpers/JWT.js'
 
 const router = Router()
@@ -15,5 +15,6 @@ router.patch('/nuevo-password/:token', nuevoPassword)
 router.post('/registro-profesor', verificarAutenticacion, verificarRolAdmin, registrarProfesor)
 router.post('/registro-asistencia', verificarAutenticacion, verificarRolProfesor, registroAsistenciaEstudiantes)
 router.post('/registro-estudiante', verificarAutenticacion, verificarRolProfesor, registrarEstudiantes)
+router,post('/registro-nota', verificarAutenticacion, verificarRolProfesor, subirNotasEstudiantes)
 
 export default router
