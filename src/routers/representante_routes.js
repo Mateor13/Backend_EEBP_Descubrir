8691038da7aa6ad4c4d51167b12357fed63c4aa2
Mateor13 <mateo.torres@epn.edu.ec>
2025-03-24 23:52:42 -0500

    Refactor validaciones en rutas de representante; reemplazar validadores específicos por un validador de ID de estudiante.

--- src/routers/representante_routes.js
+++ src/routers/representante_routes.js
@@ -1,12 +1,12 @@
 import Router from 'express'
 import { verAsistenciaEstudiante, verEstudiantes, verNotasEstudiante, verObservacionesEstudiante } from '../controllers/representante_controller.js'
 import { verificarAutenticacion, verificarRolRepresentante } from '../middlewares/JWT.js'
-import { verAsistenciaEstudianteValidator, verNotasEstudianteValidator, verObservacionesEstudianteValidator } from '../validators/representante_validator.js'
+import { validarIdEstudiante } from '../validators/representante_validator.js'
 const router = Router()
 
 router.get('/estudiantes-registrados', verificarAutenticacion, verificarRolRepresentante, verEstudiantes)
-router.get('/ver-notas-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, verNotasEstudianteValidator, verNotasEstudiante)
-router.get('/ver-observaciones-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, verObservacionesEstudianteValidator, verObservacionesEstudiante)
-router.get('/ver-asistencia-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, verAsistenciaEstudianteValidator, verAsistenciaEstudiante)
+router.get('/ver-notas-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, validarIdEstudiante, verNotasEstudiante)
+router.get('/ver-observaciones-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, validarIdEstudiante, verObservacionesEstudiante)
+router.get('/ver-asistencia-estudiante/:idEstudiante', verificarAutenticacion, verificarRolRepresentante, validarIdEstudiante, verAsistenciaEstudiante)
 
 export default router
--- src/validators/representante_validator.js
+++ src/validators/representante_validator.js
@@ -1,11 +1,11 @@
 import { check, validationResult } from 'express-validator';
 import estudiantes from '../models/estudiantes.js';
 
-const verNotasEstudianteValidator = [
+const validarIdEstudiante = () => [
     check('idEstudiante')
         .notEmpty()
         .withMessage('El id del estudiante es obligatorio')
-        .custom(async (idEstudiante, { req }) => {
+        .custom(async (idEstudiante) => {
             const estudiante = await estudiantes.findOne({ _id: idEstudiante });
             if (!estudiante) {
                 throw new Error('El estudiante no existe');
@@ -19,50 +19,8 @@ const verNotasEstudianteValidator = [
         }
         next();
     }
-]
-
-const verObservacionesEstudianteValidator = [
-    check('idEstudiante')
-        .notEmpty()
-        .withMessage('El id del estudiante es obligatorio')
-        .custom(async (idEstudiante, { req }) => {
-            const estudiante = await estudiantes.findOne({ _id: idEstudiante });
-            if (!estudiante) {
-                throw new Error('El estudiante no existe');
-            }
-            return true;
-        }),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ error: errors.array()[0].msg });
-        }
-        next();
-    }
-]
-
-const verAsistenciaEstudianteValidator = [
-    check('idEstudiante')
-        .notEmpty()
-        .withMessage('El id del estudiante es obligatorio')
-        .custom(async (idEstudiante, { req }) => {
-            const estudiante = await estudiantes.findOne({ _id: idEstudiante });
-            if (!estudiante) {
-                throw new Error('El estudiante no existe');
-            }
-            return true;
-        }),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ error: errors.array()[0].msg });
-        }
-        next();
-    }
-]
+];
 
 export {
-    verNotasEstudianteValidator,
-    verObservacionesEstudianteValidator,
-    verAsistenciaEstudianteValidator
+    validarIdEstudiante
 }
